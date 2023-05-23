151bf9d1a5d55fab4fd0391afe89879dcab82d3f
FilipNedovic <filip.nedovic@epam.com>
2023-05-23 14:22:35 +0200

    fixed wrong ID setting in local storage

--- cypress/e2e/createLabel.cy.js
+++ cypress/e2e/createLabel.cy.js
@@ -9,30 +9,41 @@ let taskCode
 let boardId 
 let orgId
 describe ('label', () => {
-    before (() => {
-        cy.loginBE();
-        cy.visit ('/');
-        cy.createOrganization('Test111111').then(() => {
-            expect(window.localStorage.getItem('orgId')).to.exist;
-            cy.createScrumBoard('Test', window.localStorage.getItem('orgId'))
-            .then(() => {
-            cy.createTask(storyTask('created BEEEEEE', window.localStorage.getItem('boardId')))
-                boardId = window.localStorage.getItem('boardId');
-                orgId = window.localStorage.getItem('boardId');
-            })
-            taskCode = window.localStorage.getItem('taskCode');
-        });
-    })
-
-    after(()=>{
-        expect(window.localStorage.getItem('token')).to.exist;
-        cy.deleteOrganization(orgId);
+    before(() => {
+      cy.loginBE();
+      cy.visit("/");
+      cy.createOrganization("Test111111").then(() => {
+        expect(window.localStorage.getItem("orgId")).to.exist;
+        cy.createScrumBoard("Test", window.localStorage.getItem("orgId")).then(
+          () => {
+            cy.createTask(
+              storyTask(
+                "created BEEEEEE",
+                window.localStorage.getItem("boardId")
+              )
+            );
+            boardId = window.localStorage.getItem("boardId");
+            orgId = window.localStorage.getItem("orgId");
+          }
+        );
+        taskCode = window.localStorage.getItem("taskCode");
+      });
+    });
+    
+    it("Create, assert and delete label successfully", () => {
+      cy.log("TASK CODE");
+      cy.visit(
+        `https://cypress.vivifyscrum-stage.com/boards/${boardId}/${window.localStorage.getItem(
+          "taskCode"
+        )}`
+      );
+      cy.wait(2000);
+      labelPage.createLabel();
     });
 
-    it('Create, assert and delete label successfully', () => {
-        cy.log("TASK CODE", )
-        cy.visit(`https://cypress.vivifyscrum-stage.com/boards/${boardId}/${window.localStorage.getItem('taskCode')}`)
-        cy.wait(2000)
-        labelPage.createLabel()
-    })
+    after(() => {
+      expect(window.localStorage.getItem("token")).to.exist;
+      cy.deleteOrganization(orgId);
+    });
+
     })
